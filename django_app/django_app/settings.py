68488a80b620ec86ef8e6b920e2b7e2b8de19b3b
janisfs <janisfs@mail.ru>
2024-10-17 23:45:26 +0300

    add new code and files

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'myproject',
+    'news',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- django_app/django_app/urls.py
+++ django_app/django_app/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from myproject import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('news/' , include('news.urls')),
     path('about/', views.page1, name='about'),
     path('service/', views.page2, name='service'),
     path('products/', views.page3, name='products'),
